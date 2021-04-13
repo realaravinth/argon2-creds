2e93950b4d11a2f4cd1b7a8b959389b0129a711b
realaravinth <realaravinth@batsense.net>
2021-04-13 17:48:12 +0000

    Deploying to gh-pages from  @ 11b490b78c1b4693607b47124e1c0e4718f290f4 🚀

--- argon2_creds/config/sidebar-items.js
+++ argon2_creds/config/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Config","Credential management configuration"],["ConfigBuilder","Builder for `Config`."],["PasswordPolicy",""],["PasswordPolicyBuilder","Builder for `PasswordPolicy`."]]});
+initSidebarItems({"enum":[["ConfigBuilderError","Error type for ConfigBuilder"],["PasswordPolicyBuilderError","Error type for PasswordPolicyBuilder"]],"struct":[["Config","Credential management configuration"],["ConfigBuilder","Builder for `Config`."],["PasswordPolicy",""],["PasswordPolicyBuilder","Builder for `PasswordPolicy`."]]});
--- implementors/core/marker/trait.StructuralPartialEq.js
+++ implementors/core/marker/trait.StructuralPartialEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["argon2_creds"] = [{"text":"impl StructuralPartialEq for CredsError","synthetic":false,"types":[]}];
+implementors["argon2_creds"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralPartialEq.html\" title=\"trait core::marker::StructuralPartialEq\">StructuralPartialEq</a> for <a class=\"enum\" href=\"argon2_creds/errors/enum.CredsError.html\" title=\"enum argon2_creds::errors::CredsError\">CredsError</a>","synthetic":false,"types":["argon2_creds::errors::CredsError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
